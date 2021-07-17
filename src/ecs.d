b3c3b96670bbc6ed7e64c9bd04a3d984055541b0
ryuukk <aaa>
2021-07-17 03:44:49 +0200

    Fix a memory leak and reorganize/simplify some stuff

--- src/ecs.d
+++ src/ecs.d
@@ -12,135 +12,16 @@ import decs.memory;
 // commit: f9cf1322ddaf1e9b80349fcc2623babb79f83538
 // https://github.com/prime31/zig-ecs
 
-struct SparseSet(SparseT)
-{
-    enum page_size = 32_768;
-    enum entity_per_page = page_size / SparseT.sizeof;
+alias Entity = ulong; // 1st
+alias Index = uint;
+alias Version = uint;
 
-    Array!(SparseT[]) sparse;
-    Array!SparseT dense;
-    SparseT entity_mask;
-    Allocator* allocator;
+enum entity_mask = 0xFFFFFFFF;
+enum version_mask = 0xFFFFFFFF;
+enum entity_shift = 32u;
 
-    static SparseSet create(Allocator* allocator)
-    {
-        SparseSet ret;
-        ret.sparse = Array!(SparseT[]).createWith(allocator);
-        ret.dense = Array!(SparseT).createWith(allocator);
-        assert(ret.sparse.capacity == 16);
-        assert(ret.dense.capacity == 16);
-
-        ret.entity_mask = SparseT.max;
-        ret.allocator = allocator;
-        return ret;
-    }
-
-    void destroy()
-    {
-        sparse.expandToCapacity();
-        foreach(size_t i, array; sparse)
-        {
-            if(array)
-                sparse.allocator.free(array.ptr);
-        }
-    }
-
-    size_t page(SparseT sparse)
-    {
-        return (sparse & entity_mask) / entity_per_page;
-    }
-
-    size_t offset(SparseT sparse)
-    {
-        return sparse & (entity_per_page - 1);
-    }
-
-    SparseT index(SparseT sparse)
-    {
-        assert((contains(sparse)));
-
-        return this.sparse.get(page(sparse))[offset(sparse)];
-    }
-
-    bool contains(SparseT sparse)
-    {
-        // TODO: not sure about the null on array thing
-        // i just replaced with length  rn
-        auto curr = page(sparse);
-        return curr < this.sparse.length  &&
-        this.sparse.get(curr) != null &&
-        this.sparse.get(curr)[offset(sparse)] != SparseT.max;
-    }
-
-    void add(SparseT sparse)
-    {
-
-        assert(!contains(sparse));
-  
-        // assure(page(entt))[offset(entt)] = packed.size()
-
-        size_t pos = page(sparse);
-        size_t off = offset(sparse);
-
-        assure(pos)[off] = cast(SparseT) dense.length;
-        
-        dense.add(sparse);
-    }
-
-    void remove(SparseT sparse)
-    {
-       assert(contains(sparse));
-
-       auto curr = page(sparse);
-       auto pos = offset(sparse);
-       auto last_dense = dense.get(dense.length - 1);
-
-       dense.set(this.sparse.get(curr)[pos], last_dense);
-       this.sparse.get(page(last_dense))[offset(last_dense)] = this.sparse.get(curr)[pos];
-       this.sparse.get(curr)[pos] = SparseT.max;
-
-       dense.pop();
-    }
-
-    SparseT[] assure(size_t pos) {
-        if (pos >= sparse.length) {
-            //printf("resize! %llu\n", pos);
-            auto start_pos = sparse.length;
-            sparse.resize(pos + 1);
-            sparse.expandToCapacity();
-            sparse._items[start_pos .. $] = null;
-        }
-
-        if ( sparse._items[pos].ptr) { // TODO: not good to get stuff from u_ array
-            return sparse._items[pos];
-        }
-
-        //printf("alloc bad\n");
-        auto new_page = sparse.allocator.alloc_array!SparseT(entity_per_page); 
-        new_page[0 .. $] = SparseT.max;
-
-        sparse.set(pos, new_page);
-
-        return sparse.get(pos);
-    }
-
-    size_t len()
-    {
-        return dense.length;
-    }
-}
-
-struct EntityTraitsDefinition(EntityType, IndexType, VersionType) 
-{
-    EntityType entity_type;
-    IndexType index_type;
-    VersionType version_type;
-    /// Mask to use to get the entity index number out of an identifier
-    EntityType entity_mask = IndexType.max;
-    /// Mask to use to get the version out of an identifier
-    EntityType version_mask = VersionType.max;
-}
-
+alias Storage(T) = ComponentStorage!(T, Entity);
+alias Registry = RegistryImpl!(Entity, Index, Version);
 
 struct ComponentStorage(Component, Entity)
 {
@@ -149,6 +30,8 @@ struct ComponentStorage(Component, Entity)
     // this problem doesn't exist with D, i'll have to create tests with empty struct!!!!
 
     SparseSet!(Entity)* set;
+
+    
     Array!(Component) instances;
     Allocator* allocator;
     /// doesnt really belong here...used to denote group ownership
@@ -174,20 +57,19 @@ struct ComponentStorage(Component, Entity)
         ret.allocator = allocator;
         ret.superr = 0;
         ret.safeDeinit = (ComponentStorage* c) {
-            printf("YOOOOOO safeDeinit\n");
             c.instances.destroy();
         };
         ret.safeSwap = (ComponentStorage* c, Entity a, Entity b , bool instances_only) {
-            printf("YOOOOOO safeSwap\n");
             if(!instances_only)
             {
                 
             }
         };
         ret.safeRemoveIfContains = (ComponentStorage* c, Entity e) {
-            printf("YOOOOOO safeRemvoeIfContains\n");
-            if(c.contains(e))
+            if (c.contains(e))
+            {
                 c.remove(e);
+            }
         };
 
         return ret;
@@ -224,12 +106,14 @@ struct ComponentStorage(Component, Entity)
         // TODO: need figure out why this
         static if( is(Component == bool) )
         {
-
+            safeRemoveIfContains(&this, entity);
         }
-
-        if(contains(entity))
+        else
         {
-            remove(entity);
+            if (contains(entity))
+            {
+                remove(entity);
+            }
         }
     }
 
@@ -246,117 +130,30 @@ struct Optional(T)
     private T value;
     private bool defined = false;
 
-    void opAssign ( T rhs )
-    {
-        defined = true;
-        value = rhs;
-    }
+    //void opAssign ( T rhs )
+    //{
+    //    defined = true;
+    //    value = rhs;
+    //}
 }
 
-struct Handles(HandleType, IndexType, VersionType)
+Index extractId(Entity handle)
 {
-    HandleType[] handles;
-    IndexType append_cursor = 0;
-    Optional!IndexType last_destroyed;
+    // TODO: cast should be fine, but double check if errors occurs
+    return  handle & entity_mask;
+}
 
-    Allocator* allocator;
+Version extractVersion(Entity handle)
+{
+    // TODO: cast should be fine, but double check if errors occurs
+    return cast(Version)(handle >> entity_shift);
+}
 
-    enum invalid_id = IndexType.max;
-
-    static Handles createWith(Allocator* allocator, size_t capacity = 32)
-    {
-        Handles ret;
-        ret.handles = allocator.alloc_array!HandleType(capacity);
-        ret.allocator = allocator;
-        return ret;
-    }
-
-    bool alive(HandleType handle)
-    {
-        auto id = extractId(handle);
-        return id < append_cursor && handles[id] == handle;
-    }
-
-    HandleType create()
-    {
-        //printf("# create entity\n");
-        if(!last_destroyed.defined)
-        {
-            //printf("\tlast destroyed null\n");
-            // ensure capacity and grow if needed
-            if(handles.length - 1 == append_cursor)
-            {
-                //printf("\treallocate handles\n");
-                handles = allocator.reallocate_array(handles.ptr, handles.length * 2);
-            }
-
-            auto id = append_cursor;
-            auto handle = forge(append_cursor, 0);
-            handles[id] = handle;
-
-            append_cursor += 1;
-
-            
-            //printf("\tnew handle: %llu\n", handle);
-            return handle;
-        }
-
-        auto versionn = extractVersion(handles[last_destroyed.value]);
-        auto destroyed_id =  extractId(handles[last_destroyed.value]);
-
-        auto handle = forge(last_destroyed.value, versionn);
-        handles[last_destroyed.value] = handle;
-
-        // TODO: redo this optional bullshit
-        last_destroyed = (destroyed_id == invalid_id) ? Optional!(IndexType)() : Optional!(IndexType)(destroyed_id, true); // destroyed_id;
-
-        //printf("\thandle: %llu, destroyed_id: %lu,  last_destroyed: %lu :: invalid: %lu\n", handle, destroyed_id, last_destroyed.value, invalid_id);
-        return handle;
-    }
-
-    void remove(HandleType handle)
-    {
-        //printf("# remove handle: %llu\n", handle);
-        //printf("\tinvalid is: %lu\n", invalid_id);
-        
-        auto id = extractId(handle);
-        if (id > append_cursor || handles[id] != handle)
-            panic("RemovedInvalidHandle");
-
-        //printf("\tid: %lu append_cursor: %lu\n", id, append_cursor);
-
-        auto next_id = (last_destroyed.defined) ? last_destroyed.value : invalid_id;
-        if (next_id == id)
-            panic("ExhaustedEntityRemoval");
-
-        //printf("\tnext_id: %lu\n", next_id);
-
-        auto versionn = extractVersion(handle);
-        handles[id] = forge(next_id, versionn + 1);
-
-        last_destroyed = id;
-
-        //printf("\tremove: %llu %lu %lu %lu %llu\n", handle, id, next_id, versionn, handles[id]);
-    }
-
-    IndexType extractId(HandleType handle)
-    {
-        // TODO: cast should be fine, but double check if errors occurs
-        return cast(IndexType) handle;
-    }
-
-    VersionType extractVersion(HandleType handle)
-    {   
-        // TODO: cast should be fine, but double check if errors occurs
-        return cast(VersionType) (handle >> (IndexType.sizeof * 8) );
-    }
-
-    HandleType forge(IndexType id, VersionType versionn)
-    {
-        // HandleType, IndexType, VersionType
-        //      ulong,      uint,        uint
-        return id | cast(HandleType) (versionn) << (IndexType.sizeof * 8);
-    }
+Entity forge(Index id, Version versionn)
+{
+    // HandleType, IndexType, VersionType
+    //      ulong,      uint,        uint
+    return id | cast(Entity)(versionn) << entity_shift;
 }
 
 struct TypeStore
@@ -373,31 +170,25 @@ struct TypeStore
     }
 }
 
-
-alias EntityTraitsDefinition_large = EntityTraitsDefinition!(ulong, uint, uint);
-alias EntityHandle_large = Handles!(ulong, uint, uint);
-alias Entity = ulong; // 1st
-alias Storage(T) = ComponentStorage!(T, Entity);
-
 struct GroupData
 {
-     ulong hash;
-     ubyte size;
-     /// optional. there will be an entity_set for non-owning groups and current for owning
-     SparseSet!(Entity) entity_set;
-     uint[] owned;
-     uint[] include;
-     uint[] exclude;
-     Registry* registry;
-     size_t current;
+    ulong hash;
+    ubyte size;
+    /// optional. there will be an entity_set for non-owning groups and current for owning
+    SparseSet!(Entity) entity_set;
+    uint[] owned;
+    uint[] include;
+    uint[] exclude;
+    Registry* registry;
+    size_t current;
 
-     static GroupData create(Allocator* allocator, Registry* registry, ulong hash, uint[] owned, uint[] include, uint[] exclude)
-     {
-         GroupData ret;
-         ret.hash = hash;
-         ret.size = cast(ubyte) (owned.length + include.length + exclude.length);
-         ret.registry = registry;
-         if(owned.length == 0)
+    static GroupData create(Allocator* allocator, Registry* registry, ulong hash, uint[] owned, uint[] include, uint[] exclude)
+    {
+        GroupData ret;
+        ret.hash = hash;
+        ret.size = cast(ubyte)(owned.length + include.length + exclude.length);
+        ret.registry = registry;
+        if (owned.length == 0)
             ret.entity_set = SparseSet!(Entity).create(allocator);
         ret.owned = dupe(allocator, owned);
         ret.include = dupe(allocator, include);
@@ -405,60 +196,135 @@ struct GroupData
         ret.registry = registry;
         ret.current = 0;
         return ret;
-     }
+    }
 }
 
-struct Registry
+struct RegistryImpl(EntityType, IndexType, VersionType)
 {
-    EntityHandle_large handles;
     HashMap!(ulong, size_t) components;
     HashMap!(uint, size_t) contexts;
     Array!(GroupData*) groups;
     TypeStore singletons;
     Allocator* allocator;
 
+    EntityType[] entities;
+    IndexType append_cursor = 0;
+    Optional!IndexType last_destroyed;
+    enum invalid_id = IndexType.max;
 
     static Registry create(Allocator* allocator)
     {
         Registry ret;
-        ret.handles = EntityHandle_large.createWith(allocator);
         ret.components = HashMap!(ulong, size_t).create(allocator);
         ret.contexts = HashMap!(uint, size_t).create(allocator);
         ret.groups = Array!(GroupData*).createWith(allocator);
         ret.singletons = TypeStore.create(allocator);
-        ret.allocator = allocator;    
+        ret.entities = allocator.alloc_array!(EntityType)(32);
+        ret.last_destroyed.value = entity_mask;
+        ret.last_destroyed.defined = false;
+        ret.allocator = allocator;
         return ret;
     }
 
     bool valid(Entity entity)
     {
-        return handles.alive(entity);
+        auto id = extractId(entity);
+        return id < append_cursor && entities[id] == entity;
+    }
+
+    Entity generate_id()
+    {
+        //ENTT_ASSERT(static_cast<typename traits_type::entity_type>(entities.size()) < traits_type::entity_mask);
+        // auto e = entity_type{static_cast<typename traits_type::entity_type>(entities.size())};
+        //return entities.emplace_back(e);
+
+        return 0;
+    }
+
+    Entity recycle_id()
+    {
+        return 0;
+    }
+
+    void release_entity(Entity entity, Version v)
+    {
     }
 
     Entity create_entity()
     {
-        return handles.create();
+        //printf("# create entity\n");
+        if (!last_destroyed.defined)
+        {
+            //printf("\tlast destroyed null\n");
+            // ensure capacity and grow if needed
+            if (entities.length - 1 == append_cursor)
+            {
+                //printf("\treallocate handles\n");
+                entities = allocator.reallocate_array(entities.ptr, entities.length * 2);
+            }
+
+            auto id = append_cursor;
+            auto handle = forge(append_cursor, 0);
+            entities[id] = handle;
+
+            append_cursor += 1;
+
+            //printf("\tnew handle: %llu\n", handle);
+            return handle;
+        }
+
+        auto versionn = extractVersion(entities[last_destroyed.value]);
+        auto destroyed_id = extractId(entities[last_destroyed.value]);
+
+        auto handle = forge(last_destroyed.value, versionn);
+        entities[last_destroyed.value] = handle;
+
+        // TODO: redo this optional bullshit
+        last_destroyed = (destroyed_id == invalid_id) ?
+            Optional!(IndexType)(entity_mask, false) : Optional!(IndexType)(destroyed_id, true);
+
+        //printf("\thandle: %llu, destroyed_id: %lu,  last_destroyed: %lu :: invalid: %lu\n", handle, destroyed_id, last_destroyed.value, invalid_id);
+        return handle;
     }
 
     void destroy_entity(Entity entity)
     {
         assert(valid(entity));
         removeAll(entity);
-        handles.remove(entity);
+
+        auto id = extractId(entity);
+        if (id > append_cursor || entities[id] != entity)
+            panic("RemovedInvalidHandle");
+
+        //printf("\tid: %lu append_cursor: %lu\n", id, append_cursor);
+
+        auto next_id = (last_destroyed.defined) ? last_destroyed.value : invalid_id;
+        if (next_id == id)
+            panic("ExhaustedEntityRemoval");
+
+        //printf("\tnext_id: %lu\n", next_id);
+
+        auto versionn = extractVersion(entity);
+        entities[id] = forge(next_id, versionn + 1);
+
+        last_destroyed = Optional!(IndexType)(id, true);
+
+        //printf("\tremove: %llu %lu %lu %lu %llu\n", handle, id, next_id, versionn, handles[id]);
+
     }
 
     void removeAll(Entity entity)
     {
         assert(valid(entity));
 
-        foreach(kv; components)
+        foreach (kv; components)
         {
             auto store = cast(Storage!(bool)*) kv.value;
             store.removeIfContains(entity);
         }
     }
 
-    void add(T)(Entity entity, T value)
+    void add(T)(Entity entity, T value = T.init)
     {
         assert(valid(entity));
         auto s = assure!T();
@@ -494,7 +360,7 @@ struct Registry
     {
         auto type_id = type_id!T;
 
-        if(components.has(type_id))
+        if (components.has(type_id))
         {
             auto ptr = components.get(type_id);
             return cast(Storage!(T)*) ptr;
@@ -516,7 +382,7 @@ struct Registry
         static if (Includes.args.length == 1 && Excludes.args.length == 0)
         {
             auto storage = assure!(Includes.args[0]);
-            return BasicView!(Includes.args[0]).create( storage );
+            return BasicView!(Includes.args[0]).create(storage);
         }
         else
         {
@@ -536,9 +402,37 @@ struct Registry
             return MultiView!(Includes.args.length, Excludes.args.length).create(&this, includes_arr, excludes_arr);
         }
     }
+/*
+    void view_it(Includes)(scope void delegate(Entity, Includes.args*) cb)
+    {
+        view_it!(Includes, Excludes!())(cb);
+    }
+
+    void view_it(Includes, Excludes)(scope void delegate(Entity, Includes.args) cb)
+    {
+        auto v = view!(Includes, Excludes)();
+        
+        foreach(e; v)
+        {
+            // i need to get types from the Includes aliasseq, so i get the component and pass them in the delegate
+            // i'm not sure how i can achieve it             
+            Includes.args* args; 
+            foreach(i, a; Includes.args)
+            {
+                args[i] = v.get!(a)(e);
+            }
+            cb(e, args);
+        }
+    }
+*/
+    // TODO: add support for groups
+    //auto group(T, Includes, Excludes)()
+    //{
+    //}
 }
 struct Includes(Args...) { alias args = Args; }
 struct Excludes(Args...) { alias args = Args; }
+struct Results(Args...) { alias args = Args; }
 
 struct BasicView(T)
 {
@@ -678,6 +572,19 @@ struct MultiView(size_t n_includes, size_t n_excludes)
     }
 }
 
+struct BasicGroup
+{
+    Registry* registry;
+    GroupData* group_data;
+}
+
+struct OwningGroup
+{
+    Registry* registy;
+    GroupData* group_data;
+    size_t* superr;
+}
+
 template type_id(alias type)
 {
     enum ulong type_id = hashStringFnv(__traits(identifier, type));
@@ -694,6 +601,11 @@ template type_id(alias type)
         return value;
     }
 }
+template type_name(alias type)
+{
+    enum auto type_name = __traits(identifier, type);
+}
+
 
 void sortSub(T1, T2)(T1[] items, T2[] sub_items, scope bool delegate(T1, T2) lessThan)
 {
@@ -717,6 +629,61 @@ void sortSub(T1, T2)(T1[] items, T2[] sub_items, scope bool delegate(T1, T2) les
     }
 }
 
+
+@("ecs")
+unittest
+{
+    import std.stdio: writeln;
+
+    struct Pos
+    {
+        int x, y;
+    }
+
+    struct Empty{}
+    struct Pos01{float x, y;}
+    struct Pos02{float x, y;}
+    struct Pos03{float x, y;}
+    struct Pos04{float x, y;}
+    struct Pos05{float x, y;}
+    struct Pos06{float x, y;}
+    struct Pos07{float x, y;}
+    struct Pos08{float x, y;}
+    struct Pos09{float x, y;}
+    struct Pos10{float x, y;}
+    struct Pos11{float x, y;}
+    struct Pos12{float x, y;}
+    struct Pos13{float x, y;}
+    struct Pos14{float x, y;}
+    struct Pos15{float x, y;}
+    struct Pos16{float x, y;}
+    struct Pos17{float x, y;}
+    struct Pos18{float x, y;}
+    struct Pos19{float x, y;}
+
+    auto registry = Registry.create(MALLOCATOR.ptr);
+    for(int i = 0; i < 10_000; i++)
+    {
+        Entity e = registry.create_entity();
+        //writeln("ENTITY: ",e);
+        assert(registry.valid(e));
+
+        registry.add(e,Pos01(5,6));
+        registry.add(e,Pos02(5,6));
+        registry.add(e,Pos03(5,6));
+        registry.add(e,Pos04(5,6));
+        registry.add(e,Pos05(5,6));
+
+        assert(registry.has!(Pos01)(e));
+        assert(registry.has!(Pos02)(e));
+        assert(registry.has!(Pos03)(e));
+        assert(registry.has!(Pos04)(e));
+        assert(registry.has!(Pos05)(e));
+    }
+
+    //registry.view_it!(Includes!(Pos01))( (Entity e, Pos01* p) {
+    //} );
+}
 
 @("ecs")
 unittest
